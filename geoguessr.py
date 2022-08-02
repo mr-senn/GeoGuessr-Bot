8f1b16f21ad2eb8dfda9b427bff9adee610ed234
Sohom Sen <sohom416@hotmail.com>
2022-08-02 16:06:39 -0400

    Update imports

--- geoguessr.py
+++ geoguessr.py
@@ -11,10 +11,12 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver import ActionChains
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.common.exceptions import *
-from inputs import maps, options, checkCustom, checkMap, checkOptions
-import time, sys
-import os
+import mysql.connector
+import time
 from os import environ
 
 # Environment varible declarations
